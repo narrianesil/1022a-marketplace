636fcd74a9613084f98a8e41d43f551e8d68c99f
narrianesil <153286088+narrianesil@users.noreply.github.com>
2024-10-22 11:23:44 -0400

    commit dos usuarios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
 import cors from 'cors'
+
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -19,6 +20,23 @@ app.get("/produtos", async(req,res)=>{
         const [result, fields] = await conection.query("SELECT * from produtos")
         await conection.end()
         //3 - Devolver os dados pra quem pediu
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Server ERROR")
+    }
+})
+
+app.get("/usuarios", async(req,res)=>{
+    try{
+        const conection = await mysql.createConnection({
+            host:process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const [result, fields] = await conection.query("SELECT * from usuarios")
+        await conection.end()
         res.send(result)
     }catch(e){
         res.status(500).send("Server ERROR")
